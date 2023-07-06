5be14c17533bd653057b9742f7a409d141061f03
nora169 <nouraibrahem395@gmail.com>
2023-07-06 23:57:23 +0300

    creating thanos project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='"it's delete half images numbers in a folder in a random way"',
+    author='me',
+    license='',
+)
